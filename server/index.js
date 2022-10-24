b3a4e2b1dd4df3b54a6130604a6e6dd1264b65f8
sajdakabir <sajda.kbir.com>
2022-10-24 18:31:07 +0530

    setup and create the form part

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ import postRoutes from './routes/posts.js';
 
 const app=express();
 
-app.use('/posts',postRoutes)
+
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+app.use('/posts',postRoutes)
 
 
 const CONNECTION_URL='mongodb+srv://sajda:sajda2001@cluster0.oumsbuw.mongodb.net/?retryWrites=true&w=majority';
