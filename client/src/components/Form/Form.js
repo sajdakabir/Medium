7c54c8528beb78e82aae4411b87c32955a12cab6
sajdakabir <sajda.kbir.com>
2022-10-29 14:55:27 +0530

    post like implemented

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -31,8 +31,8 @@ const Form = ({ currentId, setCurrentId }) => {
     }
 
     const clear = () => {
-        setCurrentId(null);
-        setPostData({creator:'',title:'',message:'',selectedFile:''});
+        setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     };
 
     return (
@@ -42,8 +42,7 @@ const Form = ({ currentId, setCurrentId }) => {
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
-
+                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
 
                 <div className={classes.fileInput}>
                     <FileBase
@@ -54,7 +53,7 @@ const Form = ({ currentId, setCurrentId }) => {
                     />
                 </div>
 
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button className={classes.buttonSubmit} style={{marginBottom:'10px'}} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
 
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -43,12 +43,14 @@ export const deletePost=async(req,res)=>{
     res.json({message:'post deleted successfully'});
 }
 
-export const likePost=async(req,res)=>{
-    const {id}=req.params;
-    if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send(`no post with this id: ${id}`);
+export const likePost = async (req, res) => {
+    const { id } = req.params;
 
-    const post=await PostMessage.findById(id);
-    const updatedPost=await PostMessage.findByIdAndUpdate(id,{likeCount:post.likeCount+1},{new:true});
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
 
-    res.json(updatedPost)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
 }
