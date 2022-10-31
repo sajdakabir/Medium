fb6ba018c930103c0b2979fa537eabedda9545f4
sajdakabir <sajda.kbir.com>
2022-10-31 15:53:20 +0530

    fixed some bugs

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { AppBar, Avatar, Toolbar, Typography ,Button} from '@material-ui/core';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import medium from '../../images/medium.png';
 
 function Navbar() {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
     const classes = useStyles();
 
-    const user=null;
+
+  useEffect(() => {
+    const token = user?.token;
+
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, []);
+    
 
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
@@ -19,7 +29,7 @@ function Navbar() {
         <Toolbar>
             { user?(
                 <div className={classes.profile}>
-                    <Avatar className={classes.purple}alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                    <Avatar className={classes.purple}alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
                     <Typography className={classes.userName} variant="h6">
                         {user.result.name}
                     </Typography>
