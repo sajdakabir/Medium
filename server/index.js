4d8550f69bf21ba64f78be1472058b52d4df2ac3
sajdakabir <sajda.kbir.com>
2022-11-01 17:08:47 +0530

    add the .env file

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app=express();
+dotenv.config(); 
 
 
 
@@ -15,10 +17,9 @@ app.use(cors());
 app.use('/posts',postRoutes)
 
 
-const CONNECTION_URL='mongodb+srv://sajda:sajda2001@cluster0.oumsbuw.mongodb.net/?retryWrites=true&w=majority';
 const PORT=process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 
